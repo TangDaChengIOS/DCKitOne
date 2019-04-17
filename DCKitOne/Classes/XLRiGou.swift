3f5e33ae1bc8260d16c0828c3f72de6cda4de633
TangDaChengIOS <454801905@qq.com>
2019-04-17 16:09:11 +0800

    “skajd”

---
+++ DCKitOne/Classes/XLRiGou.swift
@@ -0,0 +1,15 @@
+//
+//  XLRiGou.swift
+//  DCKitOne
+//
+//  Created by zzcm on 2019/4/17.
+//
+
+import UIKit
+
+public class XLRiGou: NSObject {
+    public class func test()
+    {
+        print("sdasdasdas")
+    }
+}
---
+++ Example/Pods/Target Support Files/DCKitOne/DCKitOne-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DCKitOne : NSObject
+@end
+@implementation PodsDummy_DCKitOne
+@end
